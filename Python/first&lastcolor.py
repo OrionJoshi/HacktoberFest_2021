09139821b822258544d27d64fbbbdad0140a00f8
samson027 <91814953+samson027@users.noreply.github.com>
2021-10-29 20:48:18 +0530

    Create first&lastcolor.py
    I Create first&lastcolor.py

---
+++ Python/first&lastcolor.py
@@ -0,0 +1,5 @@
+#   write a program to display the first and last colors from the following
+
+colors = ['Red','Green','White','Black']
+p = F"%s %s"%(colors[0],colors[-1])  # %s signifies that you want to add a string value into a string
+print (p)
